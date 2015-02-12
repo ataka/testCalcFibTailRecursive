74ddec779256887b85cd7166ba605865e5ee0152
ataka <masayuki.ataka@gmail.com>
2015-02-12 23:58:59 +0900

    Initial Commit

---
+++ RecFib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecFib
+//
+//  Created by 安宅 正之 on 2015/02/12.
+//  Copyright (c) 2015年 MasayukiAtaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
